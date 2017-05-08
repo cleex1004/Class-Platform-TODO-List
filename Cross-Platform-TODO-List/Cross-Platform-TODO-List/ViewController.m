59b1d607555000787aa2cc82572bcc86402e75a8
cleex1004 <cleex1004@gmail.com>
2017-05-08 12:07:58 -0700

    set up monitoring

--- Cross-Platform-TODO-List/Cross-Platform-TODO-List/ViewController.m
+++ Cross-Platform-TODO-List/Cross-Platform-TODO-List/ViewController.m
@@ -9,8 +9,14 @@
 #import "ViewController.h"
 #import "LoginViewController.h"
 @import FirebaseAuth;
+@import Firebase;
 
 @interface ViewController ()
+
+@property(strong, nonatomic) FIRDatabaseReference *userReference;
+@property(strong, nonatomic) FIRUser *currentUser;
+
+@property(nonatomic) FIRDatabaseHandle allTodosHandler;
 
 @end
 
@@ -26,10 +32,38 @@
 }
 
 -(void)checkUserStatus {
-    if (![[FIRAuth auth]currentUser]) {
+    if (![[FIRAuth auth] currentUser]) {
         LoginViewController *loginController = [self.storyboard instantiateViewControllerWithIdentifier:@"LoginViewController"];
         [self presentViewController:loginController animated:YES completion:nil];
+    } else {
+        [self setupFirebase];
+        [self startMonitoringTodoUpdates];
     }
+}
+
+-(void)setupFirebase {
+    FIRDatabaseReference *databaseReference = [[FIRDatabase database] reference];
+    self.currentUser = [[FIRAuth auth] currentUser];
+    self.userReference = [[databaseReference child:@"users"] child:self.currentUser.uid];
+    
+    NSLog(@"USER REFERENCE: %@", self.userReference);
+}
+
+-(void)startMonitoringTodoUpdates {
+    self.allTodosHandler = [[self.userReference child:@"todos"] observeEventType:FIRDataEventTypeValue withBlock:^(FIRDataSnapshot * _Nonnull snapshot) {
+        NSMutableArray *allTodos = [[NSMutableArray alloc]init];
+        
+        for (FIRDataSnapshot *child in snapshot.children) {
+            
+            NSDictionary *todoData = child.value;
+            NSString *todoTitle = todoData[@"title"];
+            NSString *todoContent = todoData[@"content"];
+            
+            //for lab append new 'todo' to allTodos array
+            
+            NSLog(@"Todo Title: %@ - Content: %@", todoTitle, todoContent);
+        }
+    }];
 }
 
 
