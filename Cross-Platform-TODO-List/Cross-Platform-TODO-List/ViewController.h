00264c4a8043a83c03c98acf05a7bcd710dd166d
cleex1004 <cleex1004@gmail.com>
2017-05-08 10:14:55 -0700

    initial set up

---
+++ Cross-Platform-TODO-List/Cross-Platform-TODO-List/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Cross-Platform-TODO-List
+//
+//  Created by Christina Lee on 5/8/17.
+//  Copyright © 2017 Christina Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Cross-Platform-TODO-List/Cross-Platform-TODO-List/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Cross-Platform-TODO-List
+//
+//  Created by Christina Lee on 5/8/17.
+//  Copyright © 2017 Christina Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
