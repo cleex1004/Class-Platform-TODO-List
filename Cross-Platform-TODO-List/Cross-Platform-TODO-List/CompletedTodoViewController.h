f68c143eaedc81270a00b620298b1a0f36c09f5b
cleex1004 <cleex1004@gmail.com>
2017-05-10 14:26:44 -0700

    set up completed, working on firebase

---
+++ Cross-Platform-TODO-List/Cross-Platform-TODO-List/CompletedTodoViewController.h
@@ -0,0 +1,13 @@
+//
+//  CompletedTodoViewController.h
+//  Cross-Platform-TODO-List
+//
+//  Created by Christina Lee on 5/10/17.
+//  Copyright © 2017 Christina Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CompletedTodoViewController : UIViewController
+
+@end
--- Cross-Platform-TODO-List/Cross-Platform-TODO-List/CompletedTodoViewController.m
+++ Cross-Platform-TODO-List/Cross-Platform-TODO-List/CompletedTodoViewController.m
@@ -1,42 +1,42 @@
 //
-//  ViewController.m
+//  CompletedTodoViewController.m
 //  Cross-Platform-TODO-List
 //
-//  Created by Christina Lee on 5/8/17.
+//  Created by Christina Lee on 5/10/17.
 //  Copyright © 2017 Christina Lee. All rights reserved.
 //
 
-#import "ViewController.h"
+#import "CompletedTodoViewController.h"
 #import "LoginViewController.h"
 #import "Todo.h"
 @import FirebaseAuth;
 @import Firebase;
 
-@interface ViewController () <UITableViewDataSource>
+@interface CompletedTodoViewController () <UITableViewDataSource>
 
 @property(strong, nonatomic) FIRDatabaseReference *userReference;
 @property(strong, nonatomic) FIRUser *currentUser;
-@property(strong, nonatomic) NSMutableArray<Todo *> *allTodos;
+@property(nonatomic) FIRDatabaseHandle completedTodosHandler;
 
-@property(nonatomic) FIRDatabaseHandle allTodosHandler;
+@property (strong, nonatomic) NSMutableArray<Todo *> *completedTodos;
 
-@property (weak, nonatomic) IBOutlet UIView *containerView;
-@property (weak, nonatomic) IBOutlet UITableView *todoTableView;
-@property (weak, nonatomic) IBOutlet NSLayoutConstraint *heightConstraint;
-
+@property (weak, nonatomic) IBOutlet UITableView *completedTable;
 
 @end
 
-@implementation ViewController
+@implementation CompletedTodoViewController
 
--(void)viewDidLoad {
+- (void)viewDidLoad {
     [super viewDidLoad];
+//    self.completedTodos = [[NSMutableArray alloc]init];
+//    self.completedTable.dataSource = self;
+    // Do any additional setup after loading the view.
 }
 
 -(void)viewDidAppear:(BOOL)animated {
     [super viewDidAppear:animated];
     [self checkUserStatus];
-    self.todoTableView.dataSource = self;
+    self.completedTable.dataSource = self;
 }
 
 -(void)checkUserStatus {
@@ -64,63 +64,36 @@
 }
 
 -(void)startMonitoringTodoUpdates {
-    self.allTodosHandler = [[self.userReference child:@"todos"] observeEventType:FIRDataEventTypeValue withBlock:^(FIRDataSnapshot * _Nonnull snapshot) {
-        self.allTodos = [[NSMutableArray<Todo *> alloc]init];
-
+    self.completedTodosHandler = [[self.userReference child:@"todos"] observeEventType:FIRDataEventTypeValue withBlock:^(FIRDataSnapshot * _Nonnull snapshot) {
+        self.completedTodos = [[NSMutableArray<Todo *> alloc]init];
+        
         for (FIRDataSnapshot *child in snapshot.children) {
             NSDictionary *todoData = child.value;
-            
+
             Todo *todo = [[Todo alloc] init];
             todo.title = todoData[@"title"];
             todo.content = todoData[@"content"];
             todo.completed = todoData[@"completed"];
             
-            [self.allTodos addObject:todo];
-            [self.todoTableView reloadData];
+            [self.completedTodos addObject:todo];
+            [self.completedTable reloadData];
             
             NSLog(@"Todo Title: %@ - Content: %@ - Completed: %@", todo.title, todo.content, todo.completed);
         }
     }];
 }
 
-- (IBAction)logoutButtonPressed:(id)sender {
-    NSError *signOutError;
-    BOOL status = [[FIRAuth auth] signOut:&signOutError];
-    if (!status) {
-        NSLog(@"Error signing out: %@", signOutError);
-        return;
-    }
-    LoginViewController *loginController = [self.storyboard instantiateViewControllerWithIdentifier:@"LoginViewController"];
-    [self presentViewController:loginController animated:YES completion:nil];
-}
-
-- (IBAction)plusButtonPressed:(id)sender {
-    if (self.heightConstraint.constant == 0) {
-//        self.containerView.hidden = NO;
-        self.heightConstraint.constant = 160;
-        [UIView animateWithDuration:0.6 animations:^{
-            [self.view layoutIfNeeded];
-        }];
-    } else {
-//        self.containerView.hidden = YES;
-        self.heightConstraint.constant = 0;
-        [UIView animateWithDuration:0.6 animations:^{
-            [self.view layoutIfNeeded];
-        }];
-    }
-}
-
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-   return [self.allTodos count];
+    return [self.completedTodos count];
 }
 
 -(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell" forIndexPath:indexPath];
-    Todo *currentTodo = self.allTodos[indexPath.row];
     
-    cell.textLabel.text = [NSString stringWithFormat:@"Todo Title: %@ - Content: %@ - Completed: %@", currentTodo.title, currentTodo.content, currentTodo.completed];
-    
+    Todo *current = self.completedTodos[indexPath.row];
+    NSString *todoTitle = current.title;
+    NSString *todoContent = current.content;
+    cell.textLabel.text = [NSString stringWithFormat:@"Todo Title: %@ - Content: %@", todoTitle, todoContent];
     return cell;
 }
-
 @end
