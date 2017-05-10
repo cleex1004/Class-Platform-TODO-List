9efc8a1bdd1298a840f4ea0bb2f9a6bd5bf849ec
cleex1004 <cleex1004@gmail.com>
2017-05-10 11:07:47 -0700

    break in lecture

--- Cross-Platform-TODO-List/TV-TODO-List/FirebaseAPI.m
+++ Cross-Platform-TODO-List/TV-TODO-List/FirebaseAPI.m
@@ -12,8 +12,34 @@
 @implementation FirebaseAPI
 
 +(void)fetchAllTodos:(AllTodosCompletion)completion {
-    NSString *urlString = [NSString stringWithFormat:@"https://cross-platform-todo-list.firebaseio.com/users.json?auth=%@"];
+    NSString *urlString = [NSString stringWithFormat:@"https://cross-platform-todo-list.firebaseio.com/users.json?auth=%@", APP_KEY];
     
+    NSURL *databaseURL = [NSURL URLWithString:urlString];
+    
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration ephemeralSessionConfiguration]];
+    
+    [[session dataTaskWithURL:databaseURL completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        NSDictionary *rootObject = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
+//        NSLog(@"ROOT OBJECT:%@", rootObject);
+        
+        NSMutableArray *allTodos = [[NSMutableArray alloc]init];
+        
+        for (NSDictionary *userTodosDictionary in [rootObject allValues]) {
+            NSArray *userTodos = [userTodosDictionary[@"todos"] allValues];
+            
+            for (NSDictionary *todoDictionary in userTodos) {
+                Todo *newTodo = [[Todo alloc]init];
+                newTodo.title = todoDictionary[@"title"];
+                newTodo.content = todoDictionary[@"content"];
+                
+                [allTodos addObject:newTodo];
+            }
+        }
+        
+        if (completion) {
+            completion(allTodos);
+        }
+    }] resume];
 }
 
 @end
